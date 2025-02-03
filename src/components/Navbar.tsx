71ba0bbf4aac61627f107e803f41c16c32b2b6e0
justinmihigo <mihigojustin28@gmail.com>
2025-02-03 11:03:21 +0200

    ch(remove signin button)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -63,9 +63,6 @@ const Navbar = () => {
                   {item.title}
                 </a>
               ))}
-              <button className="w-full text-center bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
-                Sign In
-              </button>
             </div>
           </div>
         )}
